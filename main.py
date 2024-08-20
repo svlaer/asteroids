9c4317f9123bf80ec8adfc0d6f4adc3084fcf507
svlaer <25950796+svlaer@users.noreply.github.com>
2024-08-20 02:14:33 +0200

    Implement a player that can be moved about the screen

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0  # Delta time
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
+        # Update objects
+        for upd in updatable:
+            upd.update(dt)
+
+        # Draw to screen
         screen.fill((0, 0, 0))
+
+        for drw in drawable:
+            drw.draw(screen)
+
         pygame.display.flip()
+
+        # Limit framerate
+        dt = clock.tick(60) / 1000
 
 
 #     print('Starting asteroids!')
